555501f6241df4872902866d121148320c5ea35c
Rafael Francisco <rafaelfrancisco6@gmail.com>
2020-05-11 11:37:55 +0100

    Add min watchos version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SKTUtils",
+    platforms: [
+        .watchOS(.v3)
+    ],
     products: [
         .library(
             name: "SKTUtils",
